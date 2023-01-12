9d4da890aaf973819e4a30790ce0ea720086e2f3
Donna Kuang <donna.kuang@alarispro.com>
2023-01-12 13:35:02 -0500

    validated user input & styles on guess cards

--- app.js
+++ app.js
@@ -6,24 +6,47 @@ document.addEventListener("DOMContentLoaded", () => {
     const guessBankCorrect = document.querySelector(".guess-bank[data-type=correct]");
     const guessBankIncorrect = document.querySelector(".guess-bank[data-type=incorrect]");
     let guessedList = new Array;
+
+    // TODO 
+    // create word bank
+    // show a word to guess
+    // show correct guesses
+    // show man hanging progression
+
+
+
+
+
     
-    // prevent page from refreshing
     submitButton.addEventListener("click", (event) => {
+        // prevent page from refreshing
         event.preventDefault();
         event.stopPropagation();
         
         // get user's guess
-        let guessedLetter = userInput.value;
+        let guessedLetter = userInput.value.toUpperCase();
+        
+        // validate not empty
+        // only add unique letters -- no duplicates
+        // TODO only allow letters
+        if (guessedLetter && !guessedList.includes(guessedLetter)) {
+            
+            // append to list
+            guessedList.push(guessedLetter);
 
-        guessedList.push(`<p class="card">${guessedLetter}</p>`);
+            // loop through list to display guessed letters
+            guessBankIncorrect.innerHTML += `<p class="card">${guessedList[guessedList.length - 1]}</p>`;
+            
+        }
 
-        // loop through list to display guessed letters
-        guessedList.forEach(item => {
-            // TODO fix display bug
-            guessBankIncorrect.innerHTML += item;
-        })
-
+        // remove letter after guessing && focus on userInput
+        userInput.value = "";
+        userInput.focus();
+        
     })
+        
+        
+
 
 
 
