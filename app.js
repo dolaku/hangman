e839c25c8dbf3180eaad4d30ce0e7c36b61a1f69
Donna Kuang <donna.kuang@alarispro.com>
2023-01-26 12:52:19 -0500

    handle guess function complete with minor validation issues

--- app.js
+++ app.js
@@ -36,6 +36,17 @@ document.addEventListener("DOMContentLoaded", () => {
         event.preventDefault();
         event.stopPropagation();
         
+        handleGuess();
+        handleWinLose();
+
+        // remove letter after guessing && focus on userInput
+        userInput.value = "";
+        userInput.focus();
+        
+    })
+
+    function handleGuess() {
+    
         // get user's guess
         let guessedLetter = userInput.value.toUpperCase();
         
@@ -47,36 +58,38 @@ document.addEventListener("DOMContentLoaded", () => {
             // handle guesses
             if (randomWord.includes(guessedLetter)) {
                 // show correct letters
-
-                // TODO fix resetting previously correct guesses
-                let tempCards = "";
-                for (let i = 0; i < randomWord.length; i++) {
-                    if (randomWord[i] === guessedLetter) {
-                        tempCards += `<p class="card">${guessedLetter}</p>`;
-                    } else {
-                        tempCards += `<p class="card"></p>`;
+    
+                Array.from(word.children).forEach((letter, i) => {
+                    // check if blank
+                    if (!letter.innerText) {
+                        // update correct letters
+                        if (randomWord[i] === guessedLetter) {
+                            letter.innerText = guessedLetter;
+                        }
                     }
-                }
-                word.innerHTML = tempCards;
-
+                })
+    
             } else {
-
+    
                 // append to incorrect list
                 guessedList.push(guessedLetter);
                 // loop through list to display guessed letters
                 guessBankIncorrect.innerHTML += `<p class="card">${guessedList[guessedList.length - 1]}</p>`;
-
+    
             }
-
-
-            // TODO check win/lose
-
         }
+    }
 
-        // remove letter after guessing && focus on userInput
-        userInput.value = "";
-        userInput.focus();
+    function handleWinLose() {
+        let hasBlank = false;
+        Array.from(word.children).forEach(letter => {
+            if(!letter.innerText) {
+                hasBlank = true;
+                console.log(hasBlank);
+            }
+        })
+
         
-    })
-
-});  
+    }
+    
+});
